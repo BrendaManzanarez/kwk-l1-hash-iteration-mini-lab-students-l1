902dce1130646e4f6d1c4287767c113f424db510
Brenda Manzanarez <brendamanzanarez.link@gmail.com>
2018-06-06 22:55:25 +0000

    automatically backed up by learn

--- getting_information.rb
+++ getting_information.rb
@@ -7,3 +7,10 @@ answer = gets #takes in input from the terminal with new line character (when yo
 
 game_number = "75"
 
+puts "Please input your frist name tribute"
+first_name = gets.chomp
+
+puts "please input your last name: "
+last_name = gets.chomp
+
+puts "welcome to the #{game_number} the hunger games, #{first_name} #{last_name}"
