c6f35c79c6349641c5908c6057443953227857f3
Brenda Manzanarez <brendamanzanarez.link@gmail.com>
2018-06-06 22:36:16 +0000

    automatically backed up by learn

--- summer_olympics.rb
+++ summer_olympics.rb
@@ -1,16 +1,16 @@
 
 summer_olympics = {
-"Atlanta" => 1996,
-"Sydney" => 2000,
-"Athens" => 2004,
-"Beijing" => 2008,
-"London" => 2012,
+"Atlanta" => "1996",
+"Sydney" => "2000",
+"Athens" => "2004",
+"Beijing" => "2008",
+"London" => "2012",
 }
 
 puts "The year thst the olympics in #{place} is #{year}
 
 place = ["Atlanta", "Sydney", "Athens", "Beijing", "London"]
-year = [1996, 2000, 2004, 2008, 2012]
+year = ["1996", "2000", "2004", "2008", "2012"]
 
 place.each do
 
