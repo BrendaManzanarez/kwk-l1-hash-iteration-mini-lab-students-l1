5f1bb3506604e87fdc4a1cbdcac142dded64f5ab
Brenda Manzanarez <brendamanzanarez.link@gmail.com>
2018-06-06 22:49:46 +0000

    automatically backed up by learn

--- getting_information.rb
+++ getting_information.rb
@@ -1,5 +1,9 @@
 puts "whats your favorite animal?"
-answer = gets 
-puts "I love #{answer}!"
+answer = gets #takes in input from the terminal with new line character (when you press 'enter')
+#puts "I love #{answer}!"
+
+#asnwer = gets.chomp
+#puts "I love #{answer}!"
 
 game_number = "75"
+
